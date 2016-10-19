07b82d2531d4d59c6265527eb71a3e0a8dce4bfb
Alexandre <alerario@gmail.com>
2016-10-18 22:07:45 -0200

    Modificado index.xhtml

--- src/TeCService/target/generated-sources/annotations/br/data/entity/Teste_.java
+++ src/TeCService/target/generated-sources/annotations/br/data/entity/Teste_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-13T10:16:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-18T22:01:44")
 @StaticMetamodel(Teste.class)
 public class Teste_ { 
 

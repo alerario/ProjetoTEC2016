9fe8bbc220d68cf4ae2f50161e2903cc9bf0574c
diegopn833 <dpn833@gmail.com>
2016-10-21 21:55:03 -0200

    Add files via upload
    R002 - cliente

--- script/database/create_tables.sql
+++ script/database/create_tables.sql
@@ -7,3 +7,29 @@ CREATE TABLE public.teste
     CONSTRAINT pk_teste PRIMARY KEY (codigo)
 );
 
+create table public.cliente
+(
+	cod_client integer NOT NULL,
+	nome varchar NOT NULL,
+	telefone varchar NOT NULL,
+	rua varchar,
+	numero varchar,
+	bairro varchar,
+	cidade varchar,
+	estado varchar,	
+	CONSTRAINT pk_cliente primary key (codigo)
+);
+
+create table public.pessoaJuridica
+(
+
+	cnpj varchar NOT NULL
+	
+)inherits(cliente);
+
+create table public.pessoaFisica
+(
+
+	cpf varchar NOT NULL
+	
+)inherits(cliente);

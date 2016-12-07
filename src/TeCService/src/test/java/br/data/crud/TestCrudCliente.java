bb758b6e79d1d6aa71c5e4b93ce6814613c796d3
arthurmiksza <arthurmiksza@hotmail.com>
2016-12-06 22:04:41 -0200

    Implementacao caso de teste R002 - Arthur Miksza

---
+++ src/TeCService/src/test/java/br/data/crud/TestCrudCliente.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.data.crud;
+
+import br.data.entity.Cliente;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alexandrelerario
+ */
+public class TestCrudCliente {
+    
+    public TestCrudCliente() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of SelectById method, of class CrudTeste.
+    */
+    
+    @Test
+    public void testSelectById() {
+        System.out.println("Select By Id - Client");
+        CrudCliente cli = new CrudCliente();
+        List<Cliente> allCli = cli.getAll();
+        List<Cliente> result = filtrar(allCli, 1);
+        assertEquals(1, result.size());
+        result = filtrar(allCli, 2);
+        assertEquals(1, result.size());
+        result = filtrar(allCli, 3);
+        assertEquals(1, result.size());
+    }
+    
+    public List<Cliente> filtrar(List<Cliente> c, int id) {
+        List<Cliente> temp = null;
+        for(Cliente cli: c) {
+            if (cli.getIdCliente() == id)
+                temp.add(cli);
+        }
+        return temp;
+    }
+    
+}
